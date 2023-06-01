afe13ebda7121f182d6203cc2933f29a52da643b
eddieg00 <119461197+eddieg00@users.noreply.github.com>
2023-05-31 21:23:16 -0400

    about me

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -3,7 +3,19 @@ function About() {
     return (
         <section className="about">
             <h1>About Me</h1>
-            <p>About me goes here!</p>
+            <div className="about-content">
+            <p>I am an aspiring web developer with a strong eagerness to expand my knowledge and skills in the coding field. Recently, 
+                I successfully completed a comprehensive full stack coding bootcamp at the Georgia Institute of Technology, marking a significant milestone in my professional journey.
+                 Now, I am actively seeking the next significant step to advance my career.</p>
+                
+                <p>Prior to the bootcamp, I dedicated three years to a managerial role at EcoWash Express car wash. This experience provided me with valuable skills, including efficient task management and effective problem-solving. 
+                It was during this time that my fascination with web development was kindled. Constantly confronted with novel challenges that required imaginative solutions, I realized that coding encompassed the very essence of this problem-solving mindset.
+                I invite you to explore my portfolio and examine my projects, which showcase the culmination of my dedication and hard work. Please feel free to reach out to me with any inquiries or questions you may have. I welcome opportunities for
+                 collaboration and growth in the field of web development.</p>
+                 
+                <p>By taking on this advanced level endeavor, I am committed to deepening my expertise and pushing the boundaries of my coding capabilities. I recognize that continuous learning and professional development 
+                are integral to success in this dynamic field. I am poised to embrace new challenges, contribute to innovative projects, and make a meaningful impact in the world of web development.</p>
+                </div>
         </section>
     )
 }
